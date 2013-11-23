ba4b48b46bf6de121004f15fd0fa312ee4bc6aa6
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:26:31 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/numpy/version.mk
+++ src/numpy/version.mk
@@ -1,5 +1,15 @@
-NAME    = numpy_$(ROLLCOMPILER)_py$(PYVERSION)
-VERSION = 1.7.0
-RELEASE = 0
+NAME               = numpy_$(ROLLCOMPILER)_py$(PYVERSION)
+VERSION            = 1.7.0
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = numpy
+
+SOURCE_NAME        = numpy
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = zip
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+ZIP_PKGS           = $(SOURCE_PKG)
