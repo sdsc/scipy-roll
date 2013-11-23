fdfd1412e42505cad723931b3be14a320ad99ef4
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:52:09 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/sympy/version.mk
+++ src/sympy/version.mk
@@ -1,5 +1,16 @@
-NAME    = sympy_py$(PYVERSION)
-VERSION = 0.7.3
-RELEASE = 0
+NAME               = sympy_py$(PYVERSION)
+VERSION            = 0.7.3
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = sympy
+
+SOURCE_NAME        = sympy
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
