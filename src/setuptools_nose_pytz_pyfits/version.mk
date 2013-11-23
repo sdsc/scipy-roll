4a08294f02bb75e0045b168ab91291035c857add
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:49:24 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/setuptools_nose_pytz_pyfits/version.mk
+++ src/setuptools_nose_pytz_pyfits/version.mk
@@ -1,5 +1,32 @@
-NAME    = setuptools_nose_pytz_pyfits_$(ROLLCOMPILER)_py$(PYVERSION)
-VERSION = 0.6c11
-RELEASE = 0
+NAME               = setuptools_nose_pytz_pyfits_$(ROLLCOMPILER)_py$(PYVERSION)
+VERSION            = 0.6c11
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = setuptools_nose_pytz_pyfits
+
+SOURCE_SUFFIX      = tgz
+
+NOSE_NAME          = nose
+NOSE_VERSION       = 1.2.1
+NOSE_PKG           = $(NOSE_NAME)-$(NOSE_VERSION).$(SOURCE_SUFFIX)
+NOSE_DIR           = $(NOSE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+PYFITS_NAME        = pyfits
+PYFITS_VERSION     = 3.1.1
+PYFITS_PKG         = $(PYFITS_NAME)-$(PYFITS_VERSION).$(SOURCE_SUFFIX)
+PYFITS_DIR         = $(PYFITS_PKG:%.$(SOURCE_SUFFIX)=%)
+
+PYTZ_NAME          = pytz
+PYTZ_VERSION       = 2012j
+PYTZ_PKG           = $(PYTZ_NAME)-$(PYTZ_VERSION).$(SOURCE_SUFFIX)
+PYTZ_DIR           = $(PYTZ_PKG:%.$(SOURCE_SUFFIX)=%)
+
+SETUPTOOLS_NAME     = setuptools
+SETUPTOOLS_VERSION  = 0.6c11
+SETUPTOOLS_PKG      = $(SETUPTOOLS_NAME)-$(SETUPTOOLS_VERSION).$(SOURCE_SUFFIX)
+SETUPTOOLS_DIR      = $(SETUPTOOLS_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(NOSE_PKG) $(PYFITS_PKG) $(PYTZ_PKG) $(SETUPTOOLS_PKG)
+
