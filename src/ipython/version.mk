f079f2e502be54f7d42b3c018d4eff07234c7659
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:07:59 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/ipython/version.mk
+++ src/ipython/version.mk
@@ -1,5 +1,16 @@
-NAME    = ipython_py$(PYVERSION)
-VERSION = 1.1.0
-RELEASE = 0
+NAME               = ipython_py$(PYVERSION)
+VERSION            = 1.1.0
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = ipython
+
+SOURCE_NAME        = ipython 
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
