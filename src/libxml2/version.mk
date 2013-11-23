b5ff5454dfa9d14dbf87da7dded3abf1249a304a
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:15:55 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/libxml2/version.mk
+++ src/libxml2/version.mk
@@ -1,5 +1,15 @@
-NAME    = libxml2_$(ROLLCOMPILER)_py$(PYVERSION)
-VERSION = 2.9.1
-RELEASE = 0
+NAME               = libxml2_$(ROLLCOMPILER)_py$(PYVERSION)
+VERSION            = 2.9.1
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = libxml2
+
+SOURCE_NAME        = libxml2
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
