fbcd6e49e5eb978ebf7f6f760b00d3a4c27ba151
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:38:40 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/scipy/version.mk
+++ src/scipy/version.mk
@@ -1,5 +1,16 @@
-NAME    = scipy_$(ROLLCOMPILER)_py$(PYVERSION)
-VERSION = 0.12.0
-RELEASE = 0
+NAME               = scipy_$(ROLLCOMPILER)_py$(PYVERSION)
+VERSION            = 0.12.0
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = scipy
+
+SOURCE_NAME        = scipy
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
