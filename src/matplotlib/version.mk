92644a0a2c9d96f0d5c221ee78b5c8df52b08298
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 16:20:07 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/matplotlib/version.mk
+++ src/matplotlib/version.mk
@@ -1,5 +1,15 @@
-NAME    = matplotlib_$(ROLLCOMPILER)_py$(PYVERSION)
-VERSION = 1.2.0
-RELEASE = 0
+NAME               = matplotlib_$(ROLLCOMPILER)_py$(PYVERSION)
+VERSION            = 1.2.0
+RELEASE            = 0
+RPM.EXTRAS         = "AutoReq: no"
+PKGROOT            = /opt/scipy/$(PYVERSION)
 
-RPM.EXTRAS = "AutoReq: no"
+SRC_SUBDIR         = matplotlib
+
+SOURCE_NAME        = matplotlib
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
