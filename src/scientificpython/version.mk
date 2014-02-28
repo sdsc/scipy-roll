2293af17dea4cb8b616daa85a10698f5fd65e047
Jim Hayes <jhayes@sdsc.edu>
2014-02-28 13:14:48 -0800

    Update to v2.9.3.

--- src/scientificpython/version.mk
+++ src/scientificpython/version.mk
@@ -1,12 +1,12 @@
 NAME               = scientificpython_$(ROLLCOMPILER)_py$(PYVERSION)
-VERSION            = 2.9.2
+VERSION            = 2.9.3
 RELEASE            = 0
 RPM.EXTRAS         = "AutoReq: no"
 PKGROOT            = /opt/scipy/$(PYVERSION)
 
 SRC_SUBDIR         = scientificpython
 
-SOURCE_NAME        = scientificpython
+SOURCE_NAME        = ScientificPython
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
