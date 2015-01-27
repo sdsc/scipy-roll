7c7cc065bec6963661785bfd3a55647f3f641a59
Jim Hayes <jhayes@sdsc.edu>
2015-01-27 12:11:04 -0800

    Figured out how to prevent setup.py from trying--and failing--to download and built numpy.  (It's available from the Python roll.)

--- src/matplotlib/version.mk
+++ src/matplotlib/version.mk
@@ -13,7 +13,7 @@ endif
 
 NAME           = sdsc-matplotlib_py$(PYVERSION)
 VERSION        = 1.4.2
-RELEASE        = 1
+RELEASE        = 2
 PKGROOT        = /opt/scipy/$(PYVERSION)
 
 SRC_SUBDIR     = matplotlib
