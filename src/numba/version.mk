6149ea8f4e37d24b1c6e6d7aeb962ec65acf36b7
Jerry Greenberg <jpg@sdsc.edu>
2018-11-04 19:01:42 -0800

    APS-920 multiple python versions of numba

--- src/numba/version.mk
+++ src/numba/version.mk
@@ -13,7 +13,7 @@ endif
 
 NAME                   = sdsc-numba_py$(PYVERSION)
 VERSION                = 0.35.0
-RELEASE                = 1
+RELEASE                = 2
 PKGROOT                = /opt/scipy/$(PYVERSION)
 
 SRC_SUBDIR             = numba
